698afee8c2e00510b2bd953e422e63e15a422b56
gabrielmanica <gabriel.manica@ciss.com.br>
2024-06-20 20:40:50 -0300

    ajsutes prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Inter } from 'next/font/google';
-import { cookies } from 'next/headers';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,9 +16,9 @@ export default async function RootLayout({
   return (
     <html lang="pt-br">
       <body className={`${inter.className}`}>
-          <div className="min-w-dvw flex min-h-dvh flex-col">
-            <main className="flex flex-1 flex-col gap-6 p-10">{children}</main>
-          </div>
+        <div className="min-w-dvw flex min-h-dvh flex-col">
+          <main className="flex flex-1 flex-col gap-6 p-10">{children}</main>
+        </div>
       </body>
     </html>
   );
